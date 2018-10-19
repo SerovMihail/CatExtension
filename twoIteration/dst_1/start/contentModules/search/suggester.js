4ab54e2b33f2672efe242f72dd5b55fa0d2dc8ea
SerovMikhail <serov.mikhail.job@gmail.com>
2018-10-19 19:56:13 +0200

    two more iteration for Ivan

---
+++ twoIteration/dst_1/start/contentModules/search/suggester.js
@@ -0,0 +1,207 @@
+window.AutoSuggest = function (textBox, engine, action) {
+  "use strict";
+  var self = this;
+  var interval = null;
+  var lastValue = "";
+  var m_suggestUrl = engine;
+  self.asDiv = null;
+  var selectedRow = -1;
+  var resultsLength = 0;
+  self.action = action;
+  self.init = function () {
+    textBox.addEventListener("keydown", self.keyDown, false);
+    textBox.addEventListener("keyup", self.keyUp, false);
+  }
+  self.setSuggestUrl = function (su) {
+    m_suggestUrl = su;
+  }
+  self.sprintf = function (str, params) {
+    var formatted = str;
+    for (var k in params) {
+      var v = params[k];
+      var regexp = new RegExp('\\{' + (k) + '\\}', 'gi');
+      formatted = formatted.replace(regexp, v);
+    }
+    return formatted;
+  }
+  self.getData = function (val) {
+    if (!val) {
+      if (self.asDiv != null) self.asDiv.style.display = 'none';
+      return;
+    }
+    // no data retrieves if, suggest url is empty 
+    if (m_suggestUrl === undefined || m_suggestUrl === '') { return; }
+    var hl;
+    var locale = hl = utils.locale;
+    var locale = locale.replace("_", "-");
+
+    var URL = self.sprintf(m_suggestUrl, { searchTerms: val, lang: locale, country: '' });
+
+    {
+      var xmlHttpRequest;
+      xmlHttpRequest = new XMLHttpRequest();
+      xmlHttpRequest.open("GET", URL, true);
+      xmlHttpRequest.onreadystatechange = function () {
+        if (xmlHttpRequest.readyState == 4) {
+          if (xmlHttpRequest.status == 200) {
+            var response;
+            if (lastValue == val) {
+              if (self.asDiv == null) {
+                self.asDiv = document.getElementById('search-suggestion-pad');
+              }
+              // clean previous result
+              self.asDiv.innerHTML = null;
+
+              var response = xmlHttpRequest.response;
+
+              if (typeof (SEARCH_ENGINES[user['sengine']]['SuggestParser']) == 'function') {
+                response = SEARCH_ENGINES[user['sengine']]['SuggestParser'](response)
+              }
+              else {
+                response = JSON.parse(response);
+              }
+
+              var completeSuggestion = response[1];
+              resultsLength = completeSuggestion.length;
+              for (var i = 0; i < resultsLength && i < 5; i++) {
+                var resultVal = completeSuggestion[i];
+                var row = document.createElement("li");
+
+                var pos = resultVal.indexOf(val);
+                if (pos != -1) {
+                  row.innerHTML = resultVal.substr(0, pos + val.length) + "<b>" + resultVal.substr(pos + val.length) + "</b>";
+                } else {
+                  row.textContent = resultVal;
+                }
+
+                row.setAttribute("id", "auto-suggest-row" + i);
+                row.index = i;
+                row.isRow = true;
+                row.addEventListener("mouseover", function (e) {
+                  var row = e.currentTarget;
+                  if (selectedRow != -1) document.getElementById("auto-suggest-row" + selectedRow).setAttribute("class", "");
+                  row.setAttribute("class", "selected");
+                  selectedRow = row.index;
+                }, false);
+                row.addEventListener("click", function (e) {
+                  var row = e.currentTarget;
+                  textBox.value = row.textContent;
+                  self.onSearch(row.textContent);
+                  e.preventDefault();
+                }, false);
+
+                self.asDiv.appendChild(row);
+
+              }
+              selectedRow = -1;
+              if (resultsLength == 0) {
+                self.asDiv.style.display = 'none';
+              } else {
+                self.asDiv.style.display = 'block';
+              }
+            }
+          }
+        }
+      };
+      xmlHttpRequest.send("");
+    }
+    //)
+  }
+  self.keyUp = function (e) {
+    var keyCode = e.keyCode;
+    if (keyCode != getKeyCode('ENTER')) { 
+      if ((keyCode != getKeyCode('UP_ARROW')) && (keyCode != getKeyCode('DOWN_ARROW')) && (keyCode != getKeyCode('F5'))) {
+        if (interval != null) {
+          window.clearInterval(interval)
+          interval = null;
+        }
+        lastValue = textBox.value;
+        interval = setTimeout(self.getData, 10, textBox.value)
+      }
+    }
+  }
+  self.keyDown = function (e) {
+    var keyCode = e.keyCode;
+    /*
+    if(keyCode==13){
+      
+      if(selectedRow!=-1){
+        textBox.value = document.getElementById("auto-suggest-row" + selectedRow).textContent;
+      }
+      t.onSearch(textBox.value);
+      return;
+      
+    }
+    */
+
+    // remove comments if you want to use autosuggest
+    // if ((keyCode != getKeyCode('UP_ARROW')) && (keyCode != 40)) {
+    // } else {
+    //   if (keyCode == getKeyCode('UP_ARROW')) {
+    //     if (selectedRow != -1) {
+    //       document.getElementById("auto-suggest-row" + selectedRow).setAttribute("class", "");
+    //     }
+    //     selectedRow--;
+    //     if (selectedRow < 0) {
+    //       selectedRow = resultsLength - 1;
+    //     }
+    //   } else {
+    //     if (selectedRow != -1) {
+    //       document.getElementById("auto-suggest-row" + selectedRow).setAttribute("class", "");
+    //     }
+    //     selectedRow++;
+    //     if (selectedRow >= resultsLength) {
+    //       selectedRow = 0;
+    //     }
+    //   }
+    //   var row = document.getElementById("auto-suggest-row" + selectedRow);
+    //   row.setAttribute("class", "selected");
+    //   textBox.value = row.textContent;
+    // }
+  }
+  self.documentMouseDown = function (e) {
+    if (e.explicitOriginalTarget != self.asDiv) {
+      lastValue = "--";
+      if (self.asDiv != null) {
+        self.asDiv.style.display = 'none';
+        try {
+          document.getElementById("container").style.height = "3px";
+        } catch (e) { console.log(e) }
+      }
+    }
+  }
+
+  self.setASdivPosition = function () {
+    var el = textBox;
+    var x = 0;
+    var y = textBox.offsetHeight - 1;
+    while ((el.offsetParent) && (el.tagName.toLowerCase() != 'body')) {
+      x += el.offsetLeft;
+      y += el.offsetTop;
+      el = el.offsetParent;
+    }
+    x += el.offsetLeft;
+    y += el.offsetTop;
+    if (self.asDiv != null) {
+      self.asDiv.style.left = x + "px";
+      self.asDiv.style.top = y + "px";
+    }
+  }
+
+  self.onSearch = function (val) {
+    lastValue = "--";
+    if (self.asDiv != null) {
+      self.asDiv.style.display = 'none';
+      if (document.getElementById("container")) {
+        document.getElementById("container").style.height = "29px";
+      }
+      self.action();
+    }
+  }
+
+  self.changeSuggestUrl = function (aUrl) {
+    m_suggestUrl = aUrl;
+  }
+
+  self.init();
+};
---
+++ twoIteration/src/start/contentModules/helpers/countdown.js
@@ -0,0 +1,170 @@
+window.loadCountDownModule = function (e) {
+  if (e.countDownThread) clearTimeout(e.countDownThread);
+  e.countDownThread = null;
+  var enableCountDownElem = $("#enable_countdown");
+  var setPositionElem = $("#countdown_setPosition");
+  var setTextElem = $("#countdown_setText");
+  var setTimeElem = $("#countdown_setTime");
+  var enableCountdown = function () {
+    setPositionElem.val(localStorage.getItem("countdownPosition"));
+    $(".countDown").removeClass("miniSize");
+    $(".countDown").removeClass("center_center");
+    if (localStorage.getItem("countdownPosition")) {
+      if (localStorage.getItem("countdownPosition").toLowerCase() == "bottom center") {
+        $(".countDown").removeClass("miniSize");
+      } else if (localStorage.getItem("countdownPosition").toLowerCase() == "center") {
+        $(".countDown").removeClass("miniSize");
+        $(".countDown").addClass("center_center");
+      } else {
+        $(".countDown").addClass("miniSize");
+      }
+    }
+    setTextElem.parent().parent().fadeIn();
+    setTimeElem.parent().parent().fadeIn();
+    setPositionElem.parent().parent().fadeIn();
+    $("#countdown").fadeIn();
+    setPositionElem.off("change");
+    setPositionElem.on("change", function () {
+      localStorage.setItem("countdownPosition", $(this).val());
+      var e = $(this).val().toLowerCase();
+      if (e === "bottom center") {
+        $(".countDown").removeClass("miniSize");
+        $(".countDown").removeClass("center_center");
+      } else if (e === "center") {
+        $(".countDown").removeClass("miniSize");
+        $(".countDown").addClass("center_center");
+      } else {
+        $(".countDown").addClass("miniSize");
+        $(".countDown").removeClass("center_center");
+      }
+      chrome.runtime.sendMessage({
+        changeOptions: utils.getGlobalOpt()
+      });
+    });
+    var countdownToTime = 0;
+    var countdownText = "";
+    if (localStorage.getItem("countdownToTime")) {
+      setTimeElem.val(localStorage.getItem("countdownToTime"));
+      countdownToTime = new Date(localStorage.getItem("countdownToTime")).getTime();
+    }
+    if (localStorage.getItem("countdownText")) {
+      setTextElem.val(localStorage.getItem("countdownText"));
+      countdownText = "Countdown to " + localStorage.getItem("countdownText");
+      $("#countdownTitle").text(countdownText);
+    }
+    var setTimeElem = function (e) {
+      if (e.handleObj.type == "blur" || e.keyCode == getKeyCode('ENTER')) {
+        if ($(this).val() == "") {
+          $(this).attr("type", "text");
+          $(this).val("Invalid time");
+          setTimeElem.off("focus");
+          setTimeElem.on("focus", function () {
+            $(this).attr("type", "datetime-local");
+          });
+        } else {
+          countdownToTime = new Date($(this).val()).getTime();
+          localStorage.setItem("countdownToTime", $(this).val());
+          chrome.runtime.sendMessage({
+            changeOptions: utils.getGlobalOpt()
+          });
+        }
+        if (e.keyCode == getKeyCode('ENTER')) {
+          $(this).trigger("blur");
+        }
+      }
+    };
+    var setTextElem = function (e) {
+      if (e.handleObj.type == "blur" || e.keyCode == getKeyCode('ENTER')) {
+        if ($(this).val().length > 0) {
+          countdownText = "Countdown to " + $(this).val();
+        } else {
+          countdownText = "";
+        }
+        $("#countdownTitle").text(countdownText);
+        localStorage.setItem("countdownText", $(this).val());
+        chrome.runtime.sendMessage({
+          changeOptions: utils.getGlobalOpt()
+        });
+        if (e.keyCode == getKeyCode('ENTER')) {
+          $(this).trigger("blur");
+        }
+      }
+    };
+    setTimeElem.off("blur");
+    setTimeElem.on("blur", setTimeElem);
+    setTimeElem.off("keydown");
+    setTimeElem.on("keydown", setTimeElem);
+    setTextElem.off("blur");
+    setTextElem.on("blur", setTextElem);
+    setTextElem.off("keydown");
+    setTextElem.on("keydown", setTextElem);
+    var c;
+    var s;
+    var u;
+    var d;
+    function m() {
+      var e = new Date().getTime();
+      if (e > countdownToTime) {
+        c = 0;
+        s = 0;
+        u = 0;
+        d = 0;
+      } else {
+        var o = (e - countdownToTime) / 1e3;
+        var min = 60;
+        o = Math.abs(Math.floor(o));
+        c = Math.floor(o / (24 * min * min));
+        d = o - c * 24 * min * min;
+        s = Math.floor(d / (min * min));
+        d = d - s * min * min;
+        u = Math.floor(d / min);
+        d = d - u * min;
+      }
+    }
+    function w() {
+      clearTimeout(e.countDownThread);
+      m();
+      $("#days .number").text(c < 10 ? ("0" + c).slice(-2) : c);
+      $("#hours .number").text(("0" + s).slice(-2));
+      $("#minutes .number").text(("0" + u).slice(-2));
+      $("#seconds .number").text(("0" + d).slice(-2));
+      if (localStorage.getItem("enable_countdown") == "yes") e.countDownThread = setTimeout(w, 999);
+    }
+    e.countDownThread = setTimeout(w, 1);
+    e.listAllThreads.threadCountdown = {
+      pause: function () {
+        clearInterval(e.countDownThread);
+      },
+      resume: function () {
+        w();
+      }
+    };
+  };
+  if (localStorage.getItem("enable_countdown") == "yes") {
+    enableCountDownElem.prop("checked", true);
+    enableCountdown();
+  } else {
+    enableCountDownElem.prop("checked", false);
+    setPositionElem.parent().parent().hide();
+    setTimeElem.parent().parent().hide();
+    setTextElem.parent().parent().hide();
+    $("#countdown").hide();
+  }
+  enableCountDownElem.off("change");
+  enableCountDownElem.on("change", function () {
+    if ($(this).is(":checked")) {
+      localStorage.setItem("enable_countdown", "yes");
+      enableCountdown();
+    } else {
+      localStorage.setItem("enable_countdown", "no");
+      setTextElem.parent().parent().fadeOut();
+      setTimeElem.parent().parent().fadeOut();
+      setPositionElem.parent().parent().fadeOut();
+      $("#countdown").fadeOut();
+      clearTimeout(e.countDownThread);
+    }
+    chrome.runtime.sendMessage({
+      changeOptions: utils.getGlobalOpt()
+    });
+  });
+};
