3d045e2bd198cd9c9ba3c27566957a733c4845da
SerovMikhail <serov.mikhail.job@gmail.com>
2019-04-29 21:03:47 +0200

    animations for frequently using elements

--- 29.04/entryPoint/scripts/background/handlers/common.js
+++ 29.04/entryPoint/scripts/background/handlers/common.js
@@ -1,12 +1,12 @@
 (function(e) {
   "use strict"; 
-  var getUId = function() {
-    var rand = function() {
+  var getGuid = function() {
+    var customRandomization = function() {
       return ((1 + Math.random()) * 65536 | 0).toString(16).substring(1);
     };
-    return rand() + rand() + "-" + rand() + "-" + rand() + "-" + rand() + "-" + rand() + rand() + rand();
+    return customRandomization() + customRandomization() + "-" + customRandomization() + "-" + customRandomization() + "-" + customRandomization() + "-" + customRandomization() + customRandomization() + customRandomization();
   };
-  var uId = localStorage.getItem("uid") || getUId();
+  var uId = localStorage.getItem("uid") || getGuid();
   localStorage.setItem("uid", uId);
   var userGroup = localStorage.getItem("user_group") || Math.floor(Math.random() * 10) + 1;
   localStorage.setItem("user_group", userGroup); 
--- 29.04/entryPoint/scripts/background/tips/checkTour.js
+++ 29.04/entryPoint/scripts/background/tips/checkTour.js
@@ -17,7 +17,7 @@ function navigationHandler(details) {
     if (details.transitionQualifiers.includes('from_address_bar') && tourShowInSearch && JSON.parse(tourShowInSearch)) {
 
         chrome.tabs.executeScript(details.tabId, {
-            file: '/entryPoint/scripts/vendors/JQuery.min.js'
+            file: '/entryPoint/scripts/vendor/JQuery.min.js'
         }, function () {            
 
             chrome.tabs.executeScript(details.tabId, {
